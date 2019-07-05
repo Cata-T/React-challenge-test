a91b719cd334a0ad4b18a4bafe2cc88211fc1e82
Cata001 <catalinateguiani@yahoo.com>
2019-07-05 11:14:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BeersContainer from './BeersContainer';
+
+function App() {
+  return (
+   <div>
+     <h1>PUNK BEERS</h1>
+     <BeersContainer/>
+    </div>
+  );
+}
+
+export default App;
